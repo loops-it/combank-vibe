4c8c1e3f1f0d57c5e0809047b042b512b960d2d8
tj <thinupavani@gmail.com>
2023-05-23 15:12:42 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/css';
@@ -8,6 +7,8 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import Layout from '@/components/layout';
 import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
+
 
 
 
@@ -16,10 +17,31 @@ import Link from 'next/link';
 export default function Home() {
 
 
+  useEffect(() => {
+    const getImageSlide = async () => {
+      const response = await fetch("https://it-marketing.website/vibe-backend/api/get-completed-images", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const slideImages = await response.json();
+      if (response.status !== 200) {
+        throw slideImages.error || new Error(`Request failed with status ${response.status}`);
+      }
+      const resCustomerId = slideImages
+      console.log("slide images : ",slideImages)
+    }
+    getImageSlide()
+  }, [])
+
+
+
   return (
     <>
-      
-        <Layout>
+
+      <Layout>
         <div className="container-fluid m-0 p-0 background_home">
           <div className="home_slider_container p-0 m-0 position-relative">
 
@@ -36,12 +58,12 @@ export default function Home() {
                       Interested? Check the future you NOW
                     </h2>
                     <Link href={'/getUserDetails'} className='d-flex justify-content-center align-items-center'>
-                    <button type="submit" className="mt-3" style={{
-                      backgroundColor: "#ee3035", color: "#fff", fontWeight: "bold", height: "50px", border:"solid 1px #fff", borderRadius:'6px'
-                    }}
-                    >
-                      Get Started
-                    </button></Link>
+                      <button type="submit" className="mt-3" style={{
+                        backgroundColor: "#ee3035", color: "#fff", fontWeight: "bold", height: "50px", border: "solid 1px #fff", borderRadius: '6px'
+                      }}
+                      >
+                        Get Started
+                      </button></Link>
                   </div>
                 </div>
               </div>
@@ -49,78 +71,78 @@ export default function Home() {
           </div>
         </div>
 
-        
+
         <div className="container-fluid m-0 background_slider p-0">
           <div className="slider_container p-0 m-0 position-relative">
             <a className="gallery_link" href="#">
               <h3>Gallery</h3>
             </a>
             <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            loop={true}
-            speed={3000}
-            autoplay={{
-              delay: 0,
-              disableOnInteraction: false,
-            }}
-            navigation
-          spaceBetween={0}
-          slidesPerView={4}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 0,
-            },
-            480: {
-              slidesPerView: 1,
-              spaceBetween: 0,
-            },
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 0,
-            },
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 0,
-            },
-            1336: {
-              slidesPerView: 4,
-              spaceBetween: 0,
-            },
-          }}
-        >
-          <SwiperSlide>
-            <Image src="/sliderimg-1.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-2.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-3.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-4.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-1.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-2.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-3.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image src="/sliderimg-4.png" alt="" width={250} height={250} className="img-fluid" />
-          </SwiperSlide>
-          
-        </Swiper>
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              loop={true}
+              speed={3000}
+              autoplay={{
+                delay: 0,
+                disableOnInteraction: false,
+              }}
+              navigation
+              spaceBetween={0}
+              slidesPerView={4}
+              onSlideChange={() => console.log('slide change')}
+              onSwiper={(swiper) => console.log(swiper)}
+              breakpoints={{
+                320: {
+                  slidesPerView: 1,
+                  spaceBetween: 0,
+                },
+                480: {
+                  slidesPerView: 1,
+                  spaceBetween: 0,
+                },
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 0,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 0,
+                },
+                1336: {
+                  slidesPerView: 4,
+                  spaceBetween: 0,
+                },
+              }}
+            >
+              <SwiperSlide>
+                <Image src="/sliderimg-1.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-2.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-3.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-4.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-1.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-2.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-3.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/sliderimg-4.png" alt="" width={250} height={250} className="img-fluid" />
+              </SwiperSlide>
+
+            </Swiper>
           </div>
         </div>
-        </Layout>
-      
+      </Layout>
+
     </>
   );
 }
