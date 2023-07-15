8dc5f8409c54ea1cd4f7682c2819be60973115d1
tj <thinupavani@gmail.com>
2023-07-15 20:10:53 +0530

    footer update

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -15,6 +15,12 @@ export default function Footer() {
         <Link href={"mailto:info@combank.net"}><Image width={30} height={30} src="/email.png" className="mx-2 border-radious-50 footer-icons" alt="" /></Link>
         <Link href={"https://twitter.com/ComBank_LK"}><Image width={30} height={30} src="/twitter.png" className="mx-2 border-radious-50 footer-icons" alt="" /></Link>
       </div>
+      <div className="d-flex text-white flex-row text-center icon_wrapper py-3 pb-lg-1">
+      <p className="mb-0 me-2" style={{width: "105px !important"}}>Solution by</p>
+          <Link href={"https://loopsintegrated.com/"} target="_blank" className="text-white mb-0" style={{textDecoration: "none",fontSize: "18px", width: "max-content !important"}}>
+           <Image src="/loopslogo.png" alt="logo"  className='' width={50} height={30} style={{width: "80px !important", height: "30px !important"}}/>
+          </Link>
+      </div>
       <div className="d-flex flex-row text-center icon_wrapper py-3 pb-lg-1">
         {/* <a href="#"> */}
           {/* <p className="text-white mb-0" style={{fontSize: "18px"}}> */}
